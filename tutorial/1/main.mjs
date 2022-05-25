52cb4d00ef4d6438fe1bd9353f1766df93a483e5
cosine1016 <tokizawa1016@gmail.com>
2022-05-25 22:21:54 +0900

    チュートリアル1

---
+++ tutorial/1/main.mjs
@@ -0,0 +1,8 @@
+import { getTicks } from 'game/utils';
+import { } from 'game/prototypes';
+import { } from 'game/constants';
+import { } from 'arena';
+
+export function loop() {
+    console.log('Current tick:', getTicks());
+}
