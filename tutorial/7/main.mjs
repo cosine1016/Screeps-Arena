fc7e703996d54b3116be29a2339dda8716c9f9f4
cosine1016 <tokizawa1016@gmail.com>
2022-05-25 23:28:37 +0900

    チュートリアル7

--- tutorial/7/main.mjs
+++ tutorial/7/main.mjs
@@ -1,8 +1,20 @@
-import { } from 'game/utils';
-import { } from 'game/prototypes';
-import { } from 'game/constants';
-import { } from 'arena';
+import * as utils from 'game/utils';
+import * as prototypes from 'game/prototypes';
+import * as constants from 'game/constants';
+import * as arena from 'arena';
 
+var i = 0;
 export function loop() {
-    // Your code goes here
+    var mySpawn     = utils.getObjectsByPrototype(prototypes.StructureSpawn)[0];
+    var myCreeps    = utils.getObjectsByPrototype(prototypes.Creep).filter(creep => creep.my);
+    var flags       = utils.getObjectsByPrototype(prototypes.Flag); 
+    
+    if(myCreeps.length < 2){
+        var creep = mySpawn.spawnCreep([constants.MOVE, constants.ATTACK]).object;
+        creep.target = flags[i];
+        if( i < flags.length - 1 ) { i++; }
+    }
+    myCreeps.forEach(creep => {
+        creep.moveTo(creep.target)
+    })
 }
