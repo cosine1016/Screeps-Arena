68cc89a74ad88df654f3df3fab262ba7a7efe70c
cosine1016 <tokizawa1016@gmail.com>
2022-05-25 23:09:34 +0900

    チュートリアル5

---
+++ tutorial/5/main.mjs
@@ -0,0 +1,20 @@
+import * as utils from 'game/utils';
+import * as prototypes from 'game/prototypes';
+import * as constants from 'game/constants';
+import * as arena from 'arena';
+
+export function loop() {
+    const tower = utils.getObjectsByPrototype(prototypes.StructureTower)[0];
+    if(tower.store[constants.RESOURCE_ENERGY] < 10) {
+        var myCreep = utils.getObjectsByPrototype(prototypes.Creep).find(creep => creep.my);
+        if(myCreep.store[constants.RESOURCE_ENERGY] == 0) {
+            var container = utils.getObjectsByPrototype(prototypes.StructureContainer)[0];
+            myCreep.withdraw(container, constants.RESOURCE_ENERGY);
+        } else {
+            myCreep.transfer(tower, constants.RESOURCE_ENERGY);
+        }
+    } else {
+        var target = utils.getObjectsByPrototype(prototypes.Creep).find(creep => !creep.my);
+        tower.attack(target);
+    }
+}
